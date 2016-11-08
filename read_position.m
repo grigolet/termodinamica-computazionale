90b86826b58b0b23bc68bd38601907487c5c28f8
Gianluca Rigoletti <gianluca.rigoletti@gmail.com>
2016-11-08 14:14:08 +0100

    Aggiunto calcolo energia

--- read_position.m
+++ read_position.m
@@ -22,6 +22,9 @@ r_cutoff = 3;
 %voglio trovare la distanza minimia tra una coppia di atomi dentro al 
 %file
 numero_vicini = zeros(numero_atomi, 1);
+% matrice a due dimensioni in cui il primo indice indica l'atomo e il
+% secondo indica il numero j-esimo dei suoi vicini
+elenco_vicini = zeros(numero_atomi, numero_atomi);
 for i=1:numero_atomi
     for j=i+1:numero_atomi
         distanza_x = (x0(i) - x0(j))^2;
@@ -45,7 +48,39 @@ for i=1:numero_atomi
             distanza_tot = sqrt(distanza_x + distanza_y + distanza_z);
             if distanza_tot < r_cutoff
                 numero_vicini(i) = numero_vicini(i) + 1;
+                elenco_vicini(i, numero_vicini(i)) = j;
             end
         end
     end
-end     
+end
+
+% devo calcolare l'interazione con epsilon 0.345 e sigma = 2.644
+% creo la matrice delle energie:
+phi = zeros(numero_atomi, numero_atomi);
+
+% definisco epsiolon e sigma
+epsilon = 0.345;
+sigma = 2.644;
+
+for i=1:numero_atomi
+    
+    % per ogni atomo prendo i suoi vicini
+    for j=1:numero_vicini(i)
+        
+        % per ogni vicino devo ricavare l'atomo k-esimo
+        k = elenco_vicini(i, j);
+        
+        % calcolo la distanza tra i e k
+        distanza_x = (x0(i) - x0(k))^2;
+        distanza_y = (y0(i) - y0(k))^2;
+        distanza_z = (z0(i) - z0(k))^2;
+        distanza_tot = sqrt(distanza_x + distanza_y + distanza_z);
+        
+        % calcolo l'energia tra atomo i e k
+        phi(i, k) = 4*epsilon*((sigma/distanza_tot)^(12) - (sigma/distanza_tot)^6);
+    end
+end
+
+% calcolo l'energia totale come un mezzo della somma
+energia_totale = 1/2 * sum(sum(phi));
+
